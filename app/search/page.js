d534fb107c9310e3e8c4037f0f4809d63ecdfc44
Mahdi Hassai <hassanim430@gmail.com>
2024-01-22 19:12:47 +0430

    adding search page header

--- app/search/page.js
+++ app/search/page.js
@@ -1,24 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Header from "./Header";
 
 function Search() {
   return (
     <main className="bg-gray-100 min-h-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         <Navbar />
-
-        {/* HEADER */}
-        <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-          <div className="text-left text-lg py-3 m-auto flex justify-center">
-            <input
-              className="rounded  mr-3 p-2 w-[450px]"
-              type="text"
-              placeholder="State, city or town"
-            />
-            <button className="rounded bg-red-600 px-9 py-2 text-white">
-              Let's go
-            </button>
-          </div>
-        </div>
+        <Header />
 
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
           {/* SEARCH SIDE BAR */}
