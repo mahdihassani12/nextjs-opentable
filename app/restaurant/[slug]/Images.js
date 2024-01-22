fc166d5008507d2905459149745b96f5a01c903c
Mahdi Hassai <hassanim430@gmail.com>
2024-01-22 19:35:25 +0430

    adding restaurant components

---
+++ app/restaurant/[slug]/Images.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Images() {
+  return (
+    <img
+      className="w-56 h-44 mr-1 mb-1"
+      src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
+      alt=""
+    />
+  );
+}
+
+export default Images;
---
+++ app/restaurant/[slug]/RestaurantNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function RestaurantNav() {
+  return (
+    <nav className="flex text-reg border-b pb-2">
+      <a href="" className="mr-7">
+        Overview
+      </a>
+      <a href="" className="mr-7">
+        Menu
+      </a>
+    </nav>
+  );
+}
+
+export default RestaurantNav;
