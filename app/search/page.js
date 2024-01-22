45551251671be21dc85d6c500cca3e726eb6160e
Mahdi Hassai <hassanim430@gmail.com>
2024-01-22 19:15:22 +0430

    adding search page sidebar

--- app/search/page.js
+++ app/search/page.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function Search() {
   return (
@@ -9,39 +10,8 @@ function Search() {
         <Header />
 
         <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          {/* SEARCH SIDE BAR */}
-          <div className="w-1/5">
-            <div className="border-b pb-4">
-              <h1 className="mb-2">Region</h1>
-              <p className="font-light text-reg">Toronto</p>
-              <p className="font-light text-reg">Ottawa</p>
-              <p className="font-light text-reg">Montreal</p>
-              <p className="font-light text-reg">Hamilton</p>
-              <p className="font-light text-reg">Kingston</p>
-              <p className="font-light text-reg">Niagara</p>
-            </div>
-            <div className="border-b pb-4 mt-3">
-              <h1 className="mb-2">Cuisine</h1>
-              <p className="font-light text-reg">Mexican</p>
-              <p className="font-light text-reg">Italian</p>
-              <p className="font-light text-reg">Chinese</p>
-            </div>
-            <div className="mt-3 pb-4">
-              <h1 className="mb-2">Price</h1>
-              <div className="flex">
-                <button className="border w-full text-reg font-light rounded-l p-2">
-                  $
-                </button>
-                <button className="border-r border-t border-b w-full text-reg font-light p-2">
-                  $$
-                </button>
-                <button className="border-r border-t border-b w-full text-reg font-light p-2 rounded-r">
-                  $$$
-                </button>
-              </div>
-            </div>
-          </div>
-          {/* SEARCH SIDE BAR */}
+          <Sidebar />
+
           <div className="w-5/6">
             {/* RESAURANT CAR */}
             <div className="border-b flex pb-5">
