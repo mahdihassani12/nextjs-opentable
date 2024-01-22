aacde6b8a0953bc9ddd936106468563757337d75
Mahdi Hassai <hassanim430@gmail.com>
2024-01-22 16:34:32 +0430

    adding card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
+import Card from "@/components/Card";
 
 function Home() {
   return (
@@ -11,32 +12,10 @@ function Home() {
         <main>
           <Header />
 
-          {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            {/* CARD */}
-            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-              <img
-                src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                alt=""
-                className="w-full h-36"
-              />
-              <div className="p-1">
-                <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
-                  <p className="ml-2">77 reviews</p>
-                </div>
-                <div className="flex text-reg font-light capitalize">
-                  <p className=" mr-3">Mexican</p>
-                  <p className="mr-3">$$$$</p>
-                  <p>Toronto</p>
-                </div>
-                <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-              </div>
-            </div>
-            {/* CARD */}
+            <Card />
           </div>
-          {/* CARDS */}
+          
         </main>
       </main>
     </main>
--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,11 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
 function Header() {
+  const [location, setLocation] = useState("");
+  const router = useRouter();
+
   return (
     <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
       <div className="text-center mt-10">
@@ -10,8 +17,13 @@ function Header() {
             className="rounded  mr-3 p-2 w-[450px]"
             type="text"
             placeholder="State, city or town"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
           />
-          <button className="rounded bg-red-600 px-9 py-2 text-white">
+          <button
+            className="rounded bg-red-600 px-9 py-2 text-white"
+            onClick={() => router.push(location)}
+          >
             Let's go
           </button>
         </div>
